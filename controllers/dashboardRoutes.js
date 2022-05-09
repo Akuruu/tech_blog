e9b94d6af3fd0d17fa7a8030e9841e0ef4d77503
Akuru <anjalismith0529@gmail.com>
2022-05-09 18:51:51 -0400

    added session and post attributes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,18 +2,30 @@ const router = require("express").Router();
 const { Post } = require("../models");
 const withAuth = require("../utils/auth");
 
+// shows all posts
 router.get("/", withAuth, async (req, res) => {
     try {
-        const postData = await Post.findAll();
-        res.status(200).json(postData);
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+            attributes: ['id', 'title', 'descirption', 'user_name', 'date_created']
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('all-posts', {
+            layout: 'dashboard',
+            posts,
+          });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-
-router.get("/new", withAuth, (req, res) => {
 // for showing new posts to the user
+router.get("/new", withAuth, (req, res) => {
+res.render('postData', {
+    layout: 'dashboard',
+  });
 })
 
 router.get("/edit/:id", withAuth, async (res, req) => {
