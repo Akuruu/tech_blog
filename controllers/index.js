b026929cce4d83ceb8def812eb86370118ab31ec
Akuru <anjalismith0529@gmail.com>
2022-05-09 19:59:43 -0400

    completed routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,11 @@
 const router = require("express").Router();
 
-// finish the required routes
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/api', apiRoutes);
-
 router.use("/", homeRoutes);
-// res.send();
-// finish the other needed routes with router.use()
+router.use('/dashboard', dashboardRoutes);
 
-
-//router.use("", dashboardRoutes);
-
-//router.use("", apiRoutes);
 module.exports = router;
