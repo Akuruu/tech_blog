c4d45320e14428e747bb4ad97c13c1314de4a5e9
Akuru <anjalismith0529@gmail.com>
2022-05-09 17:05:47 -0400

    created user post route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,9 +1,22 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
+// Creates a new instance of user
 router.post("/", async (req, res) => {
-    // Creating a new instance of user
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
 });
+
 
 router.post("/login", async (req, res) => {
     // User login
