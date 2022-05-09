1827ec751a5ebccfafb4cc5dfd527627b32211d1
Akuru <anjalismith0529@gmail.com>
2022-05-09 16:23:34 -0400

    edited post model

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,35 @@
-// Finish the rest of this model
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/config");
+
+class Post extends Model { }
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
