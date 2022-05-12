a75a2a0357b7d238ab71ea21f3fab5e8b9363b8a
Akuru <anjalismith0529@gmail.com>
2022-05-12 16:49:52 -0400

    added signup object into function

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,21 +1,24 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-input-signup');
-    const passwordEl = document.querySelector('#password-input-signup');
-  
-    const response = await fetch('/api/user', {
-         // Create the functionality to help create the buttons for your website.
+const signupFormHandler = async function (event) {
+  event.preventDefault();
 
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to sign up');
-    }
-  };
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
+  const usernameEl = document.querySelector('#username-input-signup');
+  const passwordEl = document.querySelector('#password-input-signup');
+
+  const response = await fetch('/api/user', {
+    method: 'POST',
+    body: JSON.stringify({ usernameEl, passwordEl }),
+    headers: { 'Content-Type': 'application/json' },
+  }
+
+  );
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to sign up');
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
