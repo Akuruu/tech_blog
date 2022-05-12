54346adb054c289023a212ce539db3fcbea05575
Akuru <anjalismith0529@gmail.com>
2022-05-12 16:38:12 -0400

    adjusted edit and delete button functions

--- public/javascript/edit.js
+++ public/javascript/edit.js
@@ -1,21 +1,39 @@
+const { response } = require("express");
+
 const postId = document.querySelector('input[name="post-id"]').value;
 
-const editFormHandler = async function(event) {
+const editFormHandler = async function (event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
   const body = document.querySelector('textarea[name="post-body"]').value;
 
-  await fetch(`/api/post/${postId}`, {
-       // Create the functionality to help create the buttons for your website.
-
+  // Edit button function
+  const response = await fetch(`/api/post/${postId}`, {
+    method: 'PUT',
+    body: JSON.stringify({ title, body }),
+    headers: { 'Content-Type': 'application/json' },
   });
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Sorry, your post could not be updated')
+  }
 
-  document.location.replace('/dashboard');
 };
 
-const deleteClickHandler = async function() {
-  await     // Create the functionality to help create the buttons for your website.
+// Delete button function.
+const deleteClickHandler = async function () {
+  const response = await fetch(`/api/post/${postId}`, {
+
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Sorry, your post could not be deleted')
+  }
 
 
   document.location.replace('/dashboard');
