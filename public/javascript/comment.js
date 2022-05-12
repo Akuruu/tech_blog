fc8c4306c75859e649c0b36d6a6986dea5440155
Akuru <anjalismith0529@gmail.com>
2022-05-12 16:41:48 -0400

    forgot to add feature

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,24 +1,24 @@
-const commentFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const postId = document.querySelector('input[name="post-id"]').value;
-    const body = document.querySelector('textarea[name="comment-body"]').value;
-  
+const commentFormHandler = async function (event) {
+  event.preventDefault();
 
-      const response = await fetch("/api/", {
-        method: "POST",
-        body: JSON.stringify({ post_id: postId, body }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (response.ok) {
-        document.location.replace(`/posts/${postId}`);
-      } else {
-        alert("Sorry, could not comment");
-      }
-    };
-  
-  document
-    .querySelector('#new-comment-form')
-    .addEventListener('submit', commentFormHandler);
+  const postId = document.querySelector('input[name="post-id"]').value;
+  const body = document.querySelector('textarea[name="comment-body"]').value;
+
+  // Comment function
+  const response = await fetch("/api/", {
+    method: "POST",
+    body: JSON.stringify({ post_id: postId, body }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.replace(`/posts/${postId}`);
+  } else {
+    alert("Sorry, could not comment");
+  }
+};
+
+document
+  .querySelector('#new-comment-form')
+  .addEventListener('submit', commentFormHandler);
